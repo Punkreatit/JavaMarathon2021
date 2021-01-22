fad268c7f955018a8f1787af41c5f847c07c86c4
Rus Polk <rus-polkovnikov@yandex.ru>
2021-01-22 12:10:02 +0300

    complete day1 tasks

--- src/main/java/day1/Task5.java
+++ src/main/java/day1/Task5.java
@@ -2,6 +2,9 @@ package day1;
 
 public class Task5 {
     public static void main(String[] args) {
+        for(int a=1980;a<=2020;a=a+4){
+            System.out.println("Олимпиада "+a+ " года");
+        }
 
     }
 }
